94507e636dbdea50504b890172e87f39d7ab7eba
Sara Marcondes <saram@fastmail.com>
2020-08-26 12:17:56 -0700

    Change all references to master branch to trunk

---
+++ packages/retarget-open-prs/index.js
@@ -0,0 +1,35 @@
+const { Octokit } = require( '@octokit/core' );
+
+async function retargetOpenPrs( repoOwner, repoName, from, to, accessToken ) {
+	const octokit = new Octokit( { auth: accessToken } );
+
+	const { data: pulls } = await octokit.request( 'GET /repos/{owner}/{repo}/pulls', {
+		owner: repoOwner,
+		repo: repoName,
+		base: from,
+	} );
+
+	console.error( pulls );
+
+	// for ( const pull of pulls ) {
+	// 	const {
+	// 		headers: { 'x-ratelimit-remaining': remainingRateLimit, 'x-ratelimit-reset': rateLimitReset },
+	// 	} = await octokit.request( 'PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
+	// 		owner: repoOwner,
+	// 		repo: repoName,
+	// 		pull_number: pull.number,
+	// 		base: to,
+	// 	} );
+
+	// 	if ( remainingRateLimit === 0 ) {
+	// 		const rateLimitResetDate = new Date( rateLimitReset * 1000 );
+	// 		// eslint-disable-next-line no-console
+	// 		console.error(
+	// 			`Hit rate limit. Wait until ${ rateLimitResetDate } and then run the command again.`
+	// 		);
+	// 		return;
+	// 	}
+	// }
+}
+
+module.exports = retargetOpenPrs;
